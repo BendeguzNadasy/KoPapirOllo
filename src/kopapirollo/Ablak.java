6169b7e3e70e55dc6e9c2a759fc2857184747f07
NadasyB <nadasy.bendeguz@gmail.com>
2021-04-26 17:20:30 +0200

    GUI kész

--- src/kopapirollo/Ablak.java
+++ src/kopapirollo/Ablak.java
@@ -27,17 +27,147 @@ public class Ablak extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnGrp = new javax.swing.ButtonGroup();
+        btnKo = new javax.swing.JButton();
+        btnOllo = new javax.swing.JButton();
+        btnPapir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        lblGepValasztasa = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        lblKorAllasa = new javax.swing.JLabel();
+        lblGepNyeresei = new javax.swing.JLabel();
+        lblJatekosNyeresei = new javax.swing.JLabel();
+        lblModKiir = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        mnuJatek = new javax.swing.JMenu();
+        mnuUjJatek = new javax.swing.JMenuItem();
+        mnuMentes = new javax.swing.JMenuItem();
+        mnuBetoltes = new javax.swing.JMenuItem();
+        mnuMod = new javax.swing.JMenu();
+        mnuMod3 = new javax.swing.JRadioButtonMenuItem();
+        mnuMod5 = new javax.swing.JRadioButtonMenuItem();
+        mnuMod7 = new javax.swing.JRadioButtonMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Kő Papír Olló");
+
+        btnKo.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        btnKo.setText("KŐ");
+
+        btnOllo.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        btnOllo.setText("OLLÓ");
+
+        btnPapir.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        btnPapir.setText("PAPÍR");
+
+        jLabel1.setText("Válassz:");
+
+        lblGepValasztasa.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        lblGepValasztasa.setText("Gép választása:");
+
+        lblKorAllasa.setText("Kör állása:");
+
+        lblGepNyeresei.setText("Gép nyerései:");
+
+        lblJatekosNyeresei.setText("Te nyeréseid:");
+
+        lblModKiir.setText("Mód: ");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblGepNyeresei)
+                    .addComponent(lblJatekosNyeresei)
+                    .addComponent(lblModKiir, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblKorAllasa, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblModKiir)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblKorAllasa)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblGepNyeresei)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblJatekosNyeresei)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        mnuJatek.setText("Játék");
+
+        mnuUjJatek.setText("Új játék");
+        mnuJatek.add(mnuUjJatek);
+
+        mnuMentes.setText("Mentés");
+        mnuJatek.add(mnuMentes);
+
+        mnuBetoltes.setText("Betöltés");
+        mnuJatek.add(mnuBetoltes);
+
+        jMenuBar1.add(mnuJatek);
+
+        mnuMod.setText("Mód");
+
+        btnGrp.add(mnuMod3);
+        mnuMod3.setSelected(true);
+        mnuMod3.setText("3 nyerésig");
+        mnuMod.add(mnuMod3);
+
+        btnGrp.add(mnuMod5);
+        mnuMod5.setText("5 nyerésig");
+        mnuMod.add(mnuMod5);
+
+        btnGrp.add(mnuMod7);
+        mnuMod7.setText("7 nyerésig");
+        mnuMod.add(mnuMod7);
+
+        jMenuBar1.add(mnuMod);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnKo, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnPapir, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnOllo, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel1)
+                            .addComponent(lblGepValasztasa))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnKo, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPapir, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnOllo, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblGepValasztasa)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +209,25 @@ public class Ablak extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup btnGrp;
+    private javax.swing.JButton btnKo;
+    private javax.swing.JButton btnOllo;
+    private javax.swing.JButton btnPapir;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblGepNyeresei;
+    private javax.swing.JLabel lblGepValasztasa;
+    private javax.swing.JLabel lblJatekosNyeresei;
+    private javax.swing.JLabel lblKorAllasa;
+    private javax.swing.JLabel lblModKiir;
+    private javax.swing.JMenuItem mnuBetoltes;
+    private javax.swing.JMenu mnuJatek;
+    private javax.swing.JMenuItem mnuMentes;
+    private javax.swing.JMenu mnuMod;
+    private javax.swing.JRadioButtonMenuItem mnuMod3;
+    private javax.swing.JRadioButtonMenuItem mnuMod5;
+    private javax.swing.JRadioButtonMenuItem mnuMod7;
+    private javax.swing.JMenuItem mnuUjJatek;
     // End of variables declaration//GEN-END:variables
 }
